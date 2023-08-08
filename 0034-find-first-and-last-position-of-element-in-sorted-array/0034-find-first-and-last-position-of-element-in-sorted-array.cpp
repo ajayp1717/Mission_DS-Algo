fbf5a1c9784d08c76e4ae55b5d162209f6deb88e
Ajay Patil <ajay.pict@gmail.com>
2023-08-09 00:46:42 +0530

    Time: 3 ms (97.43%), Space: 13.6 MB (68.65%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -11,21 +11,7 @@ public:
             else hi=mid-1;
         }
         return lo;
-
     }
-    int uBound(vector<int> &nums,int n,int k){
-        int lo=0,hi=n-1;
-        int mid;
-        while(lo<=hi){
-            mid=(lo+hi)/2;
-            if(nums[mid]>=k){
-                hi=mid-1;
-            }
-            else lo=mid+1;
-        }
-        return lo;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
         
         int n=nums.size();
@@ -33,7 +19,6 @@ public:
         int ub=lBound(nums,n,target+1)-1; //lb for nxt will be ub for curr
         if(lb==n || nums[lb]!=target)
             return {-1,-1};
-        
 
         return {lb,ub};
     }
