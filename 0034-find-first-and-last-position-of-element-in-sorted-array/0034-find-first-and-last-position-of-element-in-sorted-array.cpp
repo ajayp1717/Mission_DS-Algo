4910dc4c7a2e3498ba7cb554ff3ee320897b3c2b
Ajay Patil <ajay.pict@gmail.com>
2023-08-09 00:41:10 +0530

    Time: 7 ms (78.42%), Space: 13.6 MB (68.65%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
+    int lBound(vector<int> &nums,int n,int k){
+        int lo=0,hi=n-1;
+        int mid;
+        while(lo<=hi){
+            mid=(lo+hi)/2;
+            if(nums[mid]<k){
+                lo=mid+1;
+            }
+            else hi=mid-1;
+        }
+        return lo;
+
+    }
+    int uBound(vector<int> &nums,int n,int k){
+        int lo=0,hi=n-1;
+        int mid;
+        int ans=n;
+        while(lo<=hi){
+            mid=(lo+hi)/2;
+            if(nums[mid]>=k){
+                ans=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+        return ans;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>ans(2,-1);
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                ans[0]=i;
-                break;
-            }
-        }
-        for(int i=nums.size()-1;i>=0;i--){
-            if(nums[i]==target){
-                ans[1]=i;
-                break;
-            }
-        }
-        return ans; 
+        
+        int n=nums.size();
+        int lb=lBound(nums,n,target);
+        int ub=lBound(nums,n,target+1)-1;
+        if(lb==n || nums[lb]!=target)
+            return {-1,-1};
+        
+
+        return {lb,ub};
     }
 };
