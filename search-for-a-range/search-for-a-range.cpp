7b5e651fa9098d8527f9bd30cfab0bedaadb6283
Ajay Patil <ajay.pict@gmail.com>
2023-08-09 19:17:50 +0530

    Time: 5 ms, Memory: 13.6 MB - LeetHub

---
+++ search-for-a-range/search-for-a-range.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lBound(vector<int> &nums,int n,int k){
+        int lo=0,hi=n-1;
+        int mid;
+        while(lo<=hi){
+            mid=(lo+hi)/2;
+            if(nums[mid]<k){
+                lo=mid+1;
+            }
+            else hi=mid-1;
+        }
+        return lo;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+        int lb=lBound(nums,n,target);
+        int ub=lBound(nums,n,target+1)-1; //lb for nxt will be ub for curr
+        if(lb==n || nums[lb]!=target)
+            return {-1,-1};
+
+        return {lb,ub};
+    }
+};
