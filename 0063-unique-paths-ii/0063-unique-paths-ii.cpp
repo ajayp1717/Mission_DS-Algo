eb2649f94c388fc34003a7d962424d75579008f1
Ajay Patil <ajay.pict@gmail.com>
2023-08-12 17:35:52 +0530

    Time: 2 ms (68.17%), Space: 7.6 MB (90.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,24 +5,24 @@ public:
         int n=Grid[0].size();
 
         if(Grid[0][0]==1 || Grid[m-1][n-1]==1)return 0;
-        vector<vector<int>> dp(m,vector<int>(n,0));
         
-        dp[0][0]=Grid[0][0]==0 ? 1:0;
+        
+        Grid[0][0]=1;
 
         for(int i=1;i<n;i++){   //fill 1st row
-            dp[0][i]=Grid[0][i]==0 ? dp[0][i-1]:0;
+            Grid[0][i]=(Grid[0][i]==0 && Grid[0][i-1]==1) ? 1:0;
         }
 
         for(int j=1;j<m;j++){  //fill 1st column
-            dp[j][0]=Grid[j][0] ==0 ? dp[j-1][0] :0;
+            Grid[j][0]=(Grid[j][0] ==0 && Grid[j-1][0]==1)? 1:0;
         }
 
         for(int i=1;i<m;i++){  //fill rest of the grid
             for(int j=1;j<n;j++){
-                dp[i][j]=Grid[i][j]==0 ? dp[i-1][j]+dp[i][j-1] :0;
+                Grid[i][j]=Grid[i][j]==0 ? Grid[i-1][j]+Grid[i][j-1] :0;
             }
         }
-        return dp[m-1][n-1];
+        return Grid[m-1][n-1];
         
     }
 };
