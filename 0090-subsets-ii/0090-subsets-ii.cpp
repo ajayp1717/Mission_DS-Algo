db96480dbc14ff4971fa08c6d4280e4ad7f3f75f
Ajay Patil <ajay.pict@gmail.com>
2023-08-18 00:24:20 +0530

    Time: 0 ms (100%), Space: 7.5 MB (62.71%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> current;
-        sort(nums.begin(), nums.end());
-        generateSubsets(0, nums, current, ans);
-        return ans;
-    }
-    
-    void generateSubsets(int index, vector<int>& nums, vector<int>& current, vector<vector<int>>& ans) {
-        ans.push_back(current);
-        
-        for (int i = index; i < nums.size(); ++i) {
-            if (i > index && nums[i] == nums[i - 1]) // Skip duplicates only if starting from a new index
+    void helper(int idx,vector<int>& nums,vector<int>& ds,vector<vector<int>>& ans){
+        ans.push_back(ds);
+
+        for(int i=idx;i<nums.size();++i){
+            if(i>idx && nums[i]==nums[i-1])//duplicate case
                 continue;
-            
-            current.push_back(nums[i]);
-            generateSubsets(i + 1, nums, current, ans);
-            current.pop_back();
+            ds.push_back(nums[i]);
+            helper(i+1,nums,ds,ans);
+            ds.pop_back();
         }
+
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int> temp;
+        sort(nums.begin(),nums.end());
+        helper(0,nums,temp,ans);
+
+        return ans;
     }
 };
