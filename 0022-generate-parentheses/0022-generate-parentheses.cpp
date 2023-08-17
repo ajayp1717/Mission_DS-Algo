22b9afe7d928485c84bebbdfc6ade79fd538fcfd
Ajay Patil <ajay.pict@gmail.com>
2023-08-17 23:48:41 +0530

    Time: 12 ms (11.54%), Space: 27.1 MB (5.61%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     vector<string> ans;
+    bool isValid(string chk){
+        int lCount=0;
+        for(char x:chk){
+            if(x=='(')lCount++;
+            else lCount--;
+
+            if(lCount<0)return false;
+        }
+        return lCount==0;
+
+    }
     void helper(int opn,int cls,int n,string temp){
         if(temp.size()==2*n){
-            ans.push_back(temp);
+            if(isValid(temp))ans.push_back(temp);
             return;
         }
         if(opn<n){
             helper(opn+1,cls,n,temp+'(');
         }
-        if(cls<opn)helper(opn,cls+1,n,temp+')');
+        if(cls<n)helper(opn,cls+1,n,temp+')');
     }
     vector<string> generateParenthesis(int n) {
          helper(0,0,n,"");
