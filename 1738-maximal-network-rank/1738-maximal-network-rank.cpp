305e31a910687285ebdf98edf289cfa5ee35e869
Ajay Patil <ajay.pict@gmail.com>
2023-08-18 21:36:22 +0530

    Time: 105 ms (45.17%), Space: 42.3 MB (15.96%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.cpp
+++ 1738-maximal-network-rank/1738-maximal-network-rank.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-    // bool compArr(const vector<int>& a, const vector<int>& b){
-    //     return a.size() > b.size();
-    // }
-    bool hasEdge(int u, int v,vector<vector<int>>& adj) {
-        for (int neighbor : adj[u]) {
-            if (neighbor == v) {
-                return true;
-            }
-        }
-        return false;
-    }
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<vector<int>> adj(n);
+        vector<unordered_set<int>> adj(n);
+        vector<int>inD(n,0);
         for(auto x:roads){
-            adj[x[0]].push_back(x[1]);
-            adj[x[1]].push_back(x[0]);
+            adj[x[0]].insert(x[1]);
+            adj[x[1]].insert(x[0]);
+            inD[x[0]]++;
+            inD[x[1]]++;
         }
         int ans=0;
         for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            int temp = adj[i].size() + adj[j].size();
-            if (hasEdge(i, j, adj)) {
-                ans = std::max(ans, temp - 1);
-            } else {
-                ans = std::max(ans, temp);
+            for (int j = i + 1; j < n; ++j) {
+                int temp = adj[i].size() + adj[j].size();
+                ans= adj[i].count(j) ? max(ans,temp-1):max(ans,temp);
             }
         }
-    }
-
-    //     sort(adj.begin(),adj.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-    //     return a.size() > b.size();
-    // });
-
-        // for(auto y:adj){
-        //     for( auto x:y){
-        //         cout<<x<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-
         return ans;
-        
     }
 };
