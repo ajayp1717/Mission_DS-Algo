d54b99cfc76fc8ef3832fc99fe6f2f2b510f6481
Ajay Patil <ajay.pict@gmail.com>
2023-08-22 14:02:16 +0530

    Time: 0 ms (100%), Space: 6.1 MB (11.31%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
 
     string convertToTitle(int cN) {
-        int temp=0;
         string ans="";
         while(cN){
             cN--;   //adjust to 1-based indexing
-            temp=cN%26;
+            int temp=cN%26;
             ans=char('A'+temp)+ans;
             cN/=26;
         }
