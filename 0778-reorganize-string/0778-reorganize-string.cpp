8d1226e6d2c92c3f6f2f0ee3503a611a77ebd04c
Ajay Patil <ajay.pict@gmail.com>
2023-08-23 20:05:59 +0530

    Time: 0 ms (100%), Space: 6.4 MB (21.22%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -3,31 +3,28 @@ public:
     string reorganizeString(string s) {
         unordered_map<char,int>Mp;
         for(char c:s)Mp[c]++;
-        int tot=s.size(),maxi;
-        char maxOcc;
-
-        vector<pair<char,int>>charFreq;
-        for(auto& x:Mp){
-            charFreq.push_back(x);  
+        int tot=s.size();
+        vector<pair<char,int>>cF;
+        for(auto x:Mp){
+            cF.push_back(x);
         }
-        sort(charFreq.begin(),charFreq.end(),[](const auto& a,const auto & b){
+        sort(cF.begin(),cF.end(),[](const auto& a, const auto& b){
             return a.second>b.second;
         });
-        maxi=charFreq[0].second;
+        int maxi=cF[0].second;
         if(maxi>(tot+1)/2)return "";
         string ans(tot,'#');
-        
-        int idx = 0;
-        for (auto &entry : charFreq) {
-            while (entry.second > 0) {
-                if (idx >= tot)
-                    idx = 1;  // Start filling odd indices
-                ans[idx] = entry.first;
-                entry.second--;
-                idx += 2;
+        int idx=0;
+        for( auto& x:cF){
+            while(x.second){
+                if(idx>=tot)idx=1; //start filling odd indices;
+                ans[idx]=x.first;
+                x.second--;
+                idx+=2;
             }
         }
         return ans;
+        
         
     }
 };
