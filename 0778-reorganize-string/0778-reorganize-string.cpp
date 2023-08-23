bc7cc16737720c1db107847f349fb171ee2d5c9d
Ajay Patil <ajay.pict@gmail.com>
2023-08-23 20:55:02 +0530

    Time: 2 ms (56.28%), Space: 6.1 MB (76.79%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-   struct Compare {
-        bool operator()(const std::pair<char, int>& a, const std::pair<char, int>& b) {
-            return a.second < b.second;
-        }
-    };
-    string reorganizeString(string s) {
-        unordered_map<char,int>Mp;
-        for(char c:s)Mp[c]++;
-        int tot=s.size();
-        auto compare=[](const auto& a,const auto& b){
-            return a.second<b.second;};
-        priority_queue<pair<char,int>,vector<pair<char,int>>,Compare>pq;
-        
-        for(auto x:Mp){
-            pq.push(x);
-        }
-        
-        int maxi=pq.top().second;
-        if(maxi>(tot+1)/2)return "";
 
-        string ans(tot,'#');
-        int idx=0;
-        while( !pq.empty()){
-            auto x=pq.top();
-            pq.pop();
-            while(x.second){
-                if(idx>=tot)idx=1; //start filling odd indices;
-                ans[idx]=x.first;
-                x.second--;
-                idx+=2;
+    string reorganizeString(string s) {
+        
+        vector<int>charArr(26,0);
+        for(char c:s){
+            charArr[c-'a']++;
+        }
+        int maxi=0;
+        int letter=0;
+        for(int i=0;i<26;i++){
+            if(charArr[i]>maxi){
+                maxi=charArr[i];
+                letter=i;
             }
         }
+        if(maxi>((s.length()+1)/2))return "";
+
+        string ans=s;
+        int idx=0;
+
+        while(charArr[letter]!=0){
+            ans[idx]=char(letter + 'a');
+            idx+=2;
+            charArr[letter]--;
+        }
+       for(int i=0;i<charArr.size();i++){
+           
+           while(charArr[i]>0){
+                if(idx>=s.length()){
+                    idx=1;
+                }
+                ans[idx]=char(i +'a');
+                idx+=2;
+                charArr[i]--;
+           }
+       }
         return ans;  
     }
 };
