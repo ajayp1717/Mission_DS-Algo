daf6e65e3dc7581713601bf51fd4da4416b50a4b
Ajay Patil <ajay.pict@gmail.com>
2023-08-27 16:42:58 +0530

    Time: 21 ms (53.1%), Space: 17.7 MB (68.73%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,7 +3,6 @@ public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
         vector<int> candies(n,1);
-
         for (int i = 1; i < ratings.size(); i++) {
             if(ratings[i]>ratings[i-1]){
                 candies[i]=candies[i-1]+1;
@@ -14,9 +13,8 @@ public:
                 candies[i]=candies[i+1]+1;
             }
         }
-
         int ans=accumulate(candies.begin(),candies.end(),0);
+        
         return ans;
-
     }
 };
