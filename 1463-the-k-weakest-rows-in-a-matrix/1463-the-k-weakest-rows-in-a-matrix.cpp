57abf79ea4773fc77b5c80377d4a24d528904597
Ajay Patil <ajay.pict@gmail.com>
2023-09-18 13:54:34 +0530

    Time: 10 ms (54.38%), Space: 10.8 MB (9.15%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
+    int binSearch(vector<int>&row){
+        int lo=0,hi=row.size();
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(row[mid]==1)lo=mid+1;
+            else hi=mid;
+        }
+        return lo;
+    }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         int n=mat.size(),m=mat[0].size();
         vector<pair<int,int>> temp;
         for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==1)cnt++;
-            }
+            int cnt=binSearch(mat[i]);
             temp.push_back({cnt,i});
         }
         sort(temp.begin(),temp.end());
