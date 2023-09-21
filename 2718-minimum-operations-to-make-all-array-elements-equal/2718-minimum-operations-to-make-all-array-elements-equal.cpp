48089a8ec3bc072312ef48235adbc88f297ab666
Ajay Patil <ajay.pict@gmail.com>
2023-09-21 21:23:34 +0530

    Time: 171 ms (97.96%), Space: 81.5 MB (38.7%) - LeetHub

--- 2718-minimum-operations-to-make-all-array-elements-equal/2718-minimum-operations-to-make-all-array-elements-equal.cpp
+++ 2718-minimum-operations-to-make-all-array-elements-equal/2718-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -3,24 +3,19 @@ public:
     vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<long long> ans;
-        vector<long long> prefixSum(n + 1, 0);
 
-        for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = prefixSum[i] + (long long)nums[i];
+        vector<long long> ans, prefix(n + 1);
+        for (int i = 0; i < n; i++)
+            prefix[i + 1] = prefix[i] + (long long) nums[i];
+
+        for (int& x : queries) {
+            int i = lower_bound(nums.begin(), nums.end(), x) - nums.begin();
+            ans.push_back(1LL * x * (2 * i - n) + prefix[n] - 2 * prefix[i]);
+        // query[i] * i - prefix[i] is increments required
+        // prefix[n] - prefix[i] - query[i] * (n - i) is decrements required
+        // Total = query[i] * i - prefix[i] + prefix[n] - prefix[i] - query[i] * (n - i)
+        // Can be simplified to query[i] * (2 * i - n) + prefix[n] - 2 * prefix[i]
         }
-
-        for (int x : queries) {
-            int lowerIndex = lower_bound(nums.begin(), nums.end(), x) - nums.begin();
-            int upperIndex = upper_bound(nums.begin(), nums.end(), x) - nums.begin();
-            
-            long long lowerSum = abs(prefixSum[lowerIndex] - (long long)x * lowerIndex);
-            long long upperSum = abs(prefixSum[n] - prefixSum[upperIndex] - (long long)x * (n - upperIndex));
-
-            long long temp = lowerSum + upperSum;
-            ans.push_back(temp);
-        }
-
         return ans;
     }
 };
