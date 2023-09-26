4a3d966e6b6e7eb4131dea7f97a6ddd3cff51278
Ajay Patil <ajay.pict@gmail.com>
2023-09-27 00:53:10 +0530

    Time: 5 ms (24.56%), Space: 7.3 MB (6.8%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
+        unordered_map<char, bool> used;
+        vector<int> lastOccur(26, -1);
+        stack<char> St;
 
-        vector<int>used(26,0);
-        vector<int>lastOccur(26,-1);
-        string ans="";
-        stack<char>St;
+        string ans = "";
 
-        for(int i=0;i<s.length();i++){
-            lastOccur[s[i]-'a']=i;
+        for (int i = 0; i < s.length(); i++) {
+            lastOccur[s[i] - 'a'] = i;
         }
-        for(int i=0;i<s.length();i++){
-            char curr=s[i];
-            if(used[curr -'a'])continue;
 
-            while(!St.empty() && curr< St.top() && i< lastOccur[St.top()-'a']){
-                used[St.top()-'a']=0;
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s[i];
+            if (used[curr]) continue;
+
+            while (!St.empty() && curr < St.top() && i < lastOccur[St.top() - 'a']) {
+                used[St.top()] = false;
                 St.pop();
             }
             St.push(curr);
-            used[curr-'a']=1;
+            used[curr] = true;
         }
-        while(!St.empty()){
-            ans=St.top()+ans;
+
+        while (!St.empty()) {
+            ans = St.top() + ans;
             St.pop();
         }
+
         return ans;
     }
+
 };
